914ed2ee8cb0d53cac2628aa00bd7da1ddc661e9
lukasztukindorf <lt27037@gmail.com>
2021-10-04 11:46:06 +0200

    mobile header

--- components/header.php
+++ components/header.php
@@ -1,4 +1,36 @@
 <header class="header">
+    <div class="header__mobile">
+        <img src="assets/img/logo.png" alt="logo" class="header__mobile__logo">
+        <button class="header__search">Szukaj</button>
+        <span class="header__list__button" tabindex="1" style="display: flex;">
+            <li class="header__list__item">
+                O nas
+                <img src="assets/img/dropdown.svg" alt="ikona" class="header__list__item__icon">
+            </li>
+            <nav class="dropdown header__list__button__dropdown">
+                <ul class="dropdown__list">
+                    <a href="/onas-teatroteka.php" class="dropdown__list__link" tabindex="1">
+                        <li class="dropdown__list__item">O Teatrotece szkolnej</li>
+                    </a>
+                    <a href="/onas-zespol.php" class="dropdown__list__link" tabindex="1">
+                        <li class="dropdown__list__item">Nasz zespół</li>
+                    </a>
+                    <a href="/onas-konspekt.php" class="dropdown__list__link" tabindex="1">
+                        <li class="dropdown__list__item">Jak to działa?</li>
+                    </a>
+                    <a href="/onas-warsztaty.php" class="dropdown__list__link" tabindex="1">
+                        <li class="dropdown__list__item">Warsztaty</li>
+                    </a>
+                    <a href="/onas-kontakt.php" class="dropdown__list__link" tabindex="1">
+                        <li class="dropdown__list__item">Kontakt</li>
+                    </a>
+                    <a href="/" class="dropdown__list__link" tabindex="1">
+                        <li class="dropdown__list__item">Do pobrania</li>
+                    </a>
+                </ul>
+            </nav>
+        </span>
+    </div>
     <ul class="header__list">
         <a href="/" class="header__list__button active" tabindex="1">
             <img src="assets/img/logo.png" alt="logo" class="header__list__logo">
